ab753ec1af104ef4787250326714ce63bbc8a429
harshitraj21 <139043634+harshitraj21@users.noreply.github.com>
2023-07-23 03:22:39 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -17,7 +17,7 @@ import {
 } from "../controllers/productController.js";
 
 import formidable from "express-formidable";
-import { isAdmin, requireSignIn } from "../middlewares/authmiddleware.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
