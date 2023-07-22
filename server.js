a43b6f14e314ad4774553b0d20d8264a7655cb54
harshitraj21 <139043634+harshitraj21@users.noreply.github.com>
2023-07-23 03:14:47 +0530

    Updated

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import cors from 'cors';
 import path from 'path';
-
+import {fileURLtoToPath} from 'url';
 //Configure Env
 dotenv.config();
 
@@ -15,6 +15,9 @@ dotenv.config();
 //Database Configure
 connectDB();
 
+//esmodule fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 //Rest Object
 const app = express();
